cc2bb05e6aa71c4cfddfd8d683d1a6870a93ef5f
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-05-17 15:59:27 +0600

    basic project setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import node from 'eslint-plugin-node';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      node,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      semi: ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+    },
+    globals: {
+      process: 'readonly',
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+  },
+  prettierConfig,
+];
