bc40c1b7cb98f91e5ee7334c279eec6d466018f8
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-06-11 14:58:52 +0600

    Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,15 +2,34 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
+    // if the token is sent from the client
     if (!token) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'Your are not authorized');
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Your are not authorized!');
     }
-    next();
+
+    // check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'Your are not authorized!',
+          );
+        }
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
